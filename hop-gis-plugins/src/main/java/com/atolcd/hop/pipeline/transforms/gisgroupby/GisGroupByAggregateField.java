fb08cd3c0bc44d82cbd2b8224c3aaad444383cc9
neplan_dev <martin.heller@wienernetze.at>
2026-08-31 12:39:33 +0200

    Added missing files

---
+++ hop-gis-plugins/src/main/java/com/atolcd/hop/pipeline/transforms/gisgroupby/GisGroupByAggregateField.java
@@ -0,0 +1,89 @@
+package com.atolcd.hop.pipeline.transforms.gisgroupby;
+
+/*
+ * #%L
+ * Apache Hop GIS Plugin
+ * %%
+ * Copyright (C) 2021 Atol CD
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+import org.apache.hop.metadata.api.HopMetadataProperty;
+
+// Replaces the former parallel arrays (aggregateField[], subjectField[],
+// aggregateType[], valueField[]), so that Hop's reflection-based
+// (@HopMetadataProperty) serialization applies. The tag names correspond to
+// those of the old manual XML serialization.
+public class GisGroupByAggregateField {
+
+  @HopMetadataProperty(key = "aggregate")
+  private String aggregateField;
+
+  @HopMetadataProperty(key = "subject")
+  private String subjectField;
+
+  // Stored as a String (code, e.g. "SUM") instead of an int, so the
+  // .hpl file remains robust against internal numbering changes - just
+  // like in the old format via getTypeDesc()/getType().
+  @HopMetadataProperty(key = "type")
+  private String typeDesc;
+
+  @HopMetadataProperty(key = "valuefield")
+  private String valueField;
+
+  public GisGroupByAggregateField() {}
+
+  public GisGroupByAggregateField(
+      String aggregateField, String subjectField, String typeDesc, String valueField) {
+    this.aggregateField = aggregateField;
+    this.subjectField = subjectField;
+    this.typeDesc = typeDesc;
+    this.valueField = valueField;
+  }
+
+  public String getAggregateField() {
+    return aggregateField;
+  }
+
+  public void setAggregateField(String aggregateField) {
+    this.aggregateField = aggregateField;
+  }
+
+  public String getSubjectField() {
+    return subjectField;
+  }
+
+  public void setSubjectField(String subjectField) {
+    this.subjectField = subjectField;
+  }
+
+  public String getTypeDesc() {
+    return typeDesc;
+  }
+
+  public void setTypeDesc(String typeDesc) {
+    this.typeDesc = typeDesc;
+  }
+
+  public String getValueField() {
+    return valueField;
+  }
+
+  public void setValueField(String valueField) {
+    this.valueField = valueField;
+  }
+}
